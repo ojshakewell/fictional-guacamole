42f7cbfc65e3199ad9a277f2db66c68415862229
Brian Russell <brianrobertrussell@gmail.com>
2017-12-17 09:06:42 -0700

    added paralax features

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,12 @@
-    $(document).ready(function(){
-      $(".button-collapse").sideNav();
-      $('.carousel').carousel();
-    });
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.carousel').carousel();
+  $('.parallax').parallax();
+});
+
+var user = "brianrobertrussell";
+var host = "gmail.com";
+var link = user + "@" + host ;
+
+$("#mail-button").attr("href", "mail" + "to:" + link);
+$("#mail-button").text(link);
